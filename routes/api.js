dc199becaa6399a86576e6a912ff4d9f35646a60
Villy Yeoh <yeohvilly@gmail.com>
2023-11-06 15:00:11 -0500

    added an uuid file page in helpers folder

--- routes/api.js
+++ routes/api.js
@@ -1,26 +1,53 @@
 const api = require('express').Router();
-const { v4: uuidv4 } = require('uuid');
+const uuid = require('../helpers/uuid');
 const fs = require ('fs');
 
-// get requestto api/notes
-api.get('/api/notes', async (req, res) => {
+// get request to api/notes
+api.get('/api/notes', (req, res) => {
     res.json(`${req.method} request received`);
 
-    const displayHistory = await JSON.parse(fs.readFileSync('db/db.json', 'utf8'));
-    res.json(displayHistory);
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        res.json(data);
+        console.log(data);
+    })
+    // const displayHistory = await JSON.parse(fs.readFileSync('db/db.json', 'utf8'));
+    // res.json(displayHistory);
 
-    console.info(req.body);
+    // console.info(req.body);
 
     console.info(`${req.method} request received`);
 })
 
 api.post('/api/notes', (req, res) => {
     const displayHistory = JSON.parse(fs.readFileSync('db/db.json', 'utf8'));
-    const newFeedBack = {
-        title: req.body.title,
-        text: req.body.text,
-        id: uuidv4(),
+    const newNotes = {
+        title,
+        text,
+        id: uuid(),
     }
-    displayHistory.push(newFeedBack);
-    fs.writeFileSync('db')
+
+    // obtain existing reviews
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        }
+        else {
+            const parsedNotes = JSON.parse(data);
+
+            // add new note
+            parsedNotes.push(newNotes);
+
+            // write new notes to the file
+            fs.writeFile('./db/db.json', )
+        }
+    })
+
+    displayHistory.push(newNotes);
+    fs.writeFile('db/db.json', JSON.stringify(displayHistory));
+    console.log(displayHistory);
+    res.json(displayHistory);
+})
+
+api.delete('/api/notes/:id', (req, res) => {
+    let 
 })
